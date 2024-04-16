fbbd5511ea9b4d145ec241dfd6d189e38ec895d0
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2024-04-16 14:21:46 -0300

    primeiro commit

---
+++ banco.py
@@ -0,0 +1,65 @@
+menu = """
+O'que você deseja fazer hoje?
+
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sair
+
+=> """
+
+saldo = 0
+limite = 1000
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 5
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        excedeu_saldo = valor > saldo 
+
+        excedeu_limite = valor > limite 
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+    
+        if excedeu_saldo:
+            print("Operação falhou! Você não tem saldo suficiente para essa operação.")
+
+        elif excedeu_limite:
+            print("Operação falhou! Você não tem limite suficiente.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Você excedeu o limite maximo de salque.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+    elif opcao == "3":
+        print("\n=========== Extrato de Conta ===========")
+        print("Não foram realizadas movimentação." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("========================================")
+
+    elif opcao == "4":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
+        
